8ac3fa5a1412bc60d8f1175073d2e03dda7cf749
RashidK-03 <125174063+RashidK-03@users.noreply.github.com>
2025-05-13 04:30:58 +0500

    Update run.py

--- Website/run.py
+++ Website/run.py
@@ -1,6 +1,12 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Получаем порт из переменной окружения, если она есть, иначе по умолчанию используем порт 5000
+    port = int(os.environ.get("PORT", 5000))
+    
+    # Запускаем приложение на всех интерфейсах (0.0.0.0) с заданным портом
+    app.run(debug=True, host='0.0.0.0', port=port)
+
